360a63c2e9f0fb3986ef15f4cd77b9d8963182f1
WooHooMan43 <nrpawlicki@icloud.com>
2021-08-17 19:32:20 -0400

    Fix for Windows (again)
    *Fix for MacOS broke Windows, should actually be fixed now

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import platform
 
 # Credit to Jonathon Reinhart on StackOverflow
 def resource_path(relative_path):
-    if platform.platform() == "Windows":
+    if platform.platform().startswith("Windows"):
         """Get absolute path to resource, works for dev and for PyInstaller"""
         base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
         return os.path.join(base_path, relative_path)
